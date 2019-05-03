cdbd8c093c574779e7d3361b237f0de33731e3a0
O'Neal <bo0842372@otc.edu>
2019-05-03 16:27:00 -0500

    Changed API trying to get async.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
- const router = new VueRouter({
-    routes: [
-      // dynamic segments start with a colon
-      { path: '/restaurant/:id', component: Restaurant }
-    ]
-  })
+//  const router = new VueRouter({
+//     routes: [
+//       // dynamic segments start with a colon
+//       { path: '/restaurant/:id', component: Restaurant }
+//     ]
+//   })
