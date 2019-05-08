0881c618594c86c4962e85db3a51fde777d57173
Timmothy Lane <34077530+DefinitelyNotTim@users.noreply.github.com>
2019-05-08 15:56:54 -0500

    TWO CAT VIDEOS

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,3 +1,11 @@
 <template lang="html">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/hY7m5jjJ9mM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CQ85sUNBK7w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/hY7m5jjJ9mM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 </template>
+
+<style>
+  div{background: black}
+</style>
