069b13fd5723d1194e2de83838def6db34ea35ff
[Hannah Towle] <[hlt274@live.missouristate.edu]>
2019-05-09 17:06:28 -0500

    Added borders and centered the content

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@
       <!--<h1>Vue.js SPA</h1>-->
     </header> 
       <div>
-        <img src="src/Images/Bannertxtwhite.png" alt="Banner" style="max-width:100%; height:auto;" ></img>
+        <img src="src/Images/Bannertxtwhite.png" alt="Banner" style="max-width:100%; height:auto; border-style: solid none solid none; border-color: #296AAB; background:#3D5C7C" ></img>
       </div>
     <Nav></Nav>
     <main>
-     
 
+      <img src="src/Images/PawBorder.png" alt="border" style="max-width:100%; height:auto;"></img>
       <div class="content">
         <router-view></router-view>
       </div>
+      <img src="src/Images/PawBorder.png" alt="border" style="max-width:100%; height:auto;"></img>
     </main>
   </div>
 </template>
