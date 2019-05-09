9c1087bebda8dedb30511db72f615bf63f8151fb
Timmothy Lane <Lane509@live.missouristate.edu>
2019-05-09 09:26:08 -0500

    Moved Nav Bar to App.vue
    Moved the nav bar into the app.vue file so that it appears on every page without having to place the code into every component.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <!--<h1>Vue.js SPA</h1>-->
     </header>
     <main>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">Home</a>
+        </div>
+          <ul class="nav navbar-nav">
+          <li><a href="/randomcat">Random Cat</a></li>
+          <li><a href="/video">Cat vids</a></li>
+          </ul>
+        </div>
+    </nav>
       <div class="content">
         <router-view></router-view>
       </div>
@@ -13,5 +24,5 @@
 
 
 <style>
-
+body{background: black}
 </style>
